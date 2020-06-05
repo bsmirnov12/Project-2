050586110fc16335e80c8041942335941f338229
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-06-05 12:19:07 -0400

    add wiki_scrape file

---
+++ wiki_scrape.py
@@ -0,0 +1,8 @@
+# Independant instructions to scrape wikipedia for artist information
+# Search based on artist/band name -- wiki link
+
+# Relevant information (variables):
+# genre, image (src), wiki link, dob, origin
+
+# ---------------------------------
+# Step 1: Artist or band?
