822b26d16d83acc111cf50b14b5df670437f85b5
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-06-05 16:37:02 -0400

    wiki_scrape

--- wiki_scrape.py
+++ wiki_scrape.py
@@ -1,8 +1,48 @@
 # Independant instructions to scrape wikipedia for artist information
 # Search based on artist/band name -- wiki link
 
+from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+
+url= "https://en.wikipedia.org/wiki/The_Chainsmokers"
+
+
+
 # Relevant information (variables):
 # genre, image (src), wiki link, dob, origin
+band = []
+genre = []
+image = []
+link = []
+dob = []
+origin = []
 
 # ---------------------------------
-# Step 1: Artist or band?
+# Step 1: Get Info Table
+req = requests.get(url)
+soup = BeautifulSoup(req.content, 'html.parser')
+
+info_table = soup.find(class_="infobox")
+# print(info_table)
+members= info_table.find(text = "Members") 
+
+# ---------------------------------
+# Step 2: Artist or band?
+if info_table.find(text = "Members"):
+    band.append(True)
+    ##band has "members section"
+else:
+    band.append(False)
+    ##artist does not have members
+
+# ---------------------------------
+# Step 2: Genre 
+genre=info_table.select("tbody")[0]
+# print(genre)
+columns = info_table.findAll('td', text = re.compile('Genres'), attrs = {'class' : 'pos'})
+genres=genre.find(text="Genres")
+# print(genres)
+Tables = pd.read_html(url)
+print(Tables[0])
