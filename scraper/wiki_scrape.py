3c84ce200b1d9c4c3b24dfc53806bd0767576844
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-06-06 12:20:59 -0400

    define scrape_wikipedia function

--- scraper/wiki_scrape.py
+++ scraper/wiki_scrape.py
@@ -1,50 +1,53 @@
 # Independant instructions to scrape wikipedia for artist information
 # Search based on artist/band name -- wiki link
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
+def scrape_wikipedia(url): 
 
-url= "https://en.wikipedia.org/wiki/Lil_Wayne"
+    from bs4 import BeautifulSoup
+    import requests
+    import pandas as pd
+    print(url)
 
-# ---------------------------------
-# Step 1: Get Info Table
-req = requests.get(url)
-soup = BeautifulSoup(req.content, 'html.parser')
-artist_table=soup.find('table', class_='infobox')
+    # ---------------------------------
+    # Step 1: Get Info Table
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    artist_table=soup.find('table', class_='infobox')
 
-# Get Image info
-table_img=artist_table.find('img')['src']
-image_src= 'https:'+ table_img
-# print(image_src)
+    # Get Image info
+    table_img=artist_table.find('img')['src']
+    image_src= 'https:'+ table_img
+    # print(image_src)
 
-# set up empty dictionary(s) for band or artist
-band_dict= {'Origin':'null', 
-             'Genres':'null'
-             }
-artist_dict= {'Genres':'null',
-            'Born':'null',
-             }
+    # set up empty dictionary(s) for band or artist
+    band_dict= {'Origin':'null', 
+                'Genres':'null'
+                }
+    artist_dict= {'Genres':'null',
+                'Born':'null',
+                }
 
-# If statement to differentiate bands/artist:
-if artist_table.find(text = "Members"):
-    print('Band---')
-    
-    for item in band_dict:
-        row_text=artist_table.tr.find_next('th', string=item, scope='row').find_next('td').find_next('a').text
-        band_dict.update({item: row_text})
-    
-    
-    band_dict.update({'img':image_src,'is_band':1})
-    print(band_dict)
-    
-else:
-    print('Artist---')
-    for item in artist_dict:
+    # If statement to differentiate bands/artist:
+    if artist_table.find(text = "Members"):
+        print('Band---')
         
-        row_text=artist_table.tr.find_next('th', string=item, scope='row').find_next('td').find_next('a').text
-        artist_dict.update({item: row_text})
-    
-    bday= artist_table.tr.find_next(class_='bday').text
+        for item in band_dict:
+            row_text=artist_table.tr.find_next('th', string=item, scope='row').find_next('td').find_next('a').text
+            band_dict.update({item: row_text})
+        
+        
+        band_dict.update({'img':image_src,'is_band':1})
+        print(band_dict)
+        
+    else:
+        print('Artist---')
+        for item in artist_dict:
+            
+            row_text=artist_table.tr.find_next('th', string=item, scope='row').find_next('td').find_next('a').text
+            artist_dict.update({item: row_text})
+        
+        bday= artist_table.tr.find_next(class_='bday').text
 
-    artist_dict.update({'img':image_src, 'is_band':0, 'DOB':bday})
-    print(artist_dict)
+        artist_dict.update({'img':image_src, 'is_band':0, 'DOB':bday})
+        print(artist_dict)
+
+scrape_wikipedia('https://en.wikipedia.org/wiki/Lil_Wayne')
