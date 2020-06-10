5306142440dc8c9e0562edabf8b559d01d713dc0
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-06-10 13:22:19 -0400

    update layout

--- js/linechart.js
+++ js/linechart.js
@@ -5,32 +5,20 @@
     console.log(songData);
 
     var data = [];
-        songData.forEach(song => {
+
+    songData.forEach(song => {
         trace = {
             x: song['week'],
             y: song['score'],
             mode: 'lines',
+            type:'scatter',
             line:{
                 width:1
             }
         }
     data.push(trace);
-});
-    // define x values
-    // Use the map method with the arrow function to return all the week information
-    var weekArray = songData.map(songs =>  songs.week);
-    console.log(weekArray);
-
-    var weeksonChart = weekArray.map(song => song 
-        
-    );
-    console.log(weeksonChart)
+    });
     
-    // define y values
-    var positionArray = songData.map(songs =>  songs.position);
-    console.log(positionArray);
-
-    var highestPosition =positionArray.map(song => song);
 
     // define song titles (interactive hover element)
     var titles = data.map(songs =>  songs.id); //still need actual song title form id
@@ -41,10 +29,28 @@
     var layout = {
         showlegend: false,
         title: `Life cycle of a song on Top 100 Chart`,
-        xaxis:{title: "Weeks on Chart",tickmode:"linear"},
-        yaxis: {title: "Rank in Chart",tickmode:"linear"},
+        xaxis:{
+            title: "Weeks on Chart",
+            showgrid: false,
+            tickmode: 'linear',
+            ticks: 'inside',
+            tick0: 0,
+            dtick: 10,
+            ticklen: 7,
+            tickwidth: 2
+            },
+        yaxis: {
+            title: "Rank in Chart",
+            showgrid: false,
+            tickmode:"linear",
+            ticks: 'outside',
+            tick0: 25,
+            dtick: 25,
+            ticklen: 7,
+            tickwidth: 2
+        }
         
     };
       
-Plotly.newPlot('line', data, layout);
-});
+    Plotly.newPlot('line', data, layout);
+    });
