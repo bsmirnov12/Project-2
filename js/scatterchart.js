61b48fda21e42f70eb6d3b75fe0e759d4ccc4fbe
josedmyt <60160270+josedmyt@users.noreply.github.com>
2020-06-11 19:14:08 -0400

    adding scatter chart

--- js/scatterchart.js
+++ js/scatterchart.js
@@ -4,20 +4,32 @@
 
  //Read api
  d3.json("/api/v1.0/scatter").then(songData => {
-    
-
     console.log(songData);
-    var highestPosition= ;
-    var weeksInChart= ;
+
+    var highestPosition= []
+    var weeksInChart= [];
+    songData.forEach(song => {
+        weeksInChart.push(song["week_count"])
+        highestPosition.push(song["top_position"]);
+    });
+    
 
     var trace = {
         x: highestPosition,
-        y: weeksInChart
-        type: 'scatter'
+        y: weeksInChart,
+        type: 'scatter',
+        mode: 'markers'
     };
+
+    var layout = {
+        
+        title:'Weeks on chart vs. Highest Ranking',
+        xaxis: { title: "Weeks in chart"},
+        yaxis: {title: "Highest Ranking"}
+      };
 
     var data = [trace]
 
       
-    Plotly.newPlot('scatter', data);
+    Plotly.newPlot('scatter', data, layout);
     });
