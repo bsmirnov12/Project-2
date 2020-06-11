35ed3f1ac30c5ae6422e9d0e73ead1de663634f8
nupurgupta5292 <nupur.gupta5292@gmail.com>
2020-06-11 12:05:28 -0400

    Added new Index HTML file and code for Histogram1

--- js/histogram1.js
+++ js/histogram1.js
@@ -1,1 +1,18 @@
 // by song weeks (distribution of length in chart)
+
+ //Read api
+ d3.json("/api/v1.0/weekshist").then(songData => {
+    
+
+    console.log(songData);
+
+    var trace = {
+        x: songData,
+        type: 'histogram'
+    };
+
+    var data = [trace]
+
+      
+    Plotly.newPlot('histogram1', data);
+    });
