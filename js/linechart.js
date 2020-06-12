71e7b19062a180c807d3dde43d6e5bbfc9fef573
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-06-11 20:59:12 -0400

    Update linechart.js

---
+++ js/linechart.js
@@ -0,0 +1,94 @@
+// Revise endpoint for filtering parameters
+
+function buildPlot() {
+    var base_url = "/api/v1.0/evolution?"
+    // -------------------------------------
+    // base_endpoint='/api/v1.0/evolution?years=<comma separated list of years>&above=<int>&below=<int>&more=<int>&less=<int></int>'
+    // years - comma separeted list of years. Include only songs that were in Top 100 during specified years
+    // above - include only songs which position number was <=above (above=50 - top half of Top 100, #1 hits and below to #50)
+    // below - include only songs which position number was >=below (below=50 - bottom half of Top 100, from #50 to #100)
+    // more - include only songs which stayed in Top 100 >=more number of weeks
+    // less - include only songs which stayed in Top 100 <=less number of weeks
+    
+    var years = [2020]
+    var above = 50
+    var below = 0
+    var more = 0
+    var less = 0
+    const query1= `${base_url}years=${years}&above=${above}&below=${below}&more=${more}&less=${less}`
+
+    var data = []
+
+    
+    // Read api and handle data promise
+    d3.json(query1).then(songData => {
+
+        songData.forEach(song => {
+
+            var trace = {
+                // hovertemplate: hoverTemplate(),
+                x: song['week'],
+                y: song['score'],
+                mode: 'lines',
+                type:'scatter',
+                line:{
+                    width:1
+                },
+                opacity:0.5
+            }
+
+        data.push(trace);
+
+        });
+        var myPlot = document.getElementById('myDiv'),
+        // define layout object
+        layout = {
+            hovermode: 'closest',
+            showlegend: false,
+            title: `Life cycle of a song on Top 100 Chart (${years[0]}-${years[years.length-1]})`,
+            xaxis:{
+                title: "Weeks on Chart",
+                showgrid: false,
+                tickmode: 'linear',
+                ticks: 'inside',
+                tick0: 0,
+                dtick: 10,
+                ticklen: 7,
+                tickwidth: 2
+                },
+            yaxis: {
+                title: "Rank in Chart",
+                showgrid: false,
+                tickmode:"linear",
+                ticks: 'outside',
+                tick0: 25,
+                dtick: 25,
+                ticklen: 7,
+                tickwidth: 2
+            }
+            
+        };
+        
+        Plotly.newPlot('line', data, layout);
+        });
+}
+
+buildPlot();
+
+
+
+myPlot.on('plotly_hover', d3.json(query2 +).then(
+    songData => 
+    var pn='',
+        tn='',
+        colors=[];
+    for(var i=0; i < data.points.length; i++){
+      pn = data.points[i].pointNumber;
+      tn = data.points[i].curveNumber;
+      colors = data.points[i].data.marker.color;
+    };
+    colors[pn] = '#C54C82';
+  
+    var update = {'marker':{color: colors, size:16}};
+    Plotly.restyle('myDiv', update, [tn]);
+  });
