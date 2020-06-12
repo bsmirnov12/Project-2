4fda7416c3a55398eb537cc3dd8b1a0ea0d7587f
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-06-12 19:43:04 -0400

    add dynamic element

--- js/barchart.js
+++ js/barchart.js
@@ -1,52 +1,30 @@
-//Read api
+// define button for form
 
-    // var base_url = "/api/v1.0/artistrating?"
-    // var limit = 10
-    // var band= 0  // 1 = band, 0 = individual
-    // const query1= `${base_url}limit=${limit}&is_band=${band}`
 
-    // var data = []
-
+function handleInput() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+  
+    // Select the input value from the form
+    var button = d3.select("#artistInput").node().value;
     
-    // // Read api and handle data promise
-    // d3.json(query1).then(artistData => {
+    // Build the plot with the selected value
+    renderBarchart(button);
+  }
 
-    //     var labels=[]
-    //     var artistID= artistData["artist_id"];
-    //     var score= artistData["total_score"];
 
-    //     console.log(artistID)
-    //     // artist_name = artistID.map(artist => artist.toString()  
-    //         // );  //for now- later change artist name to actual name
-    //     // labels.push(artist_name) 
-
-    //     var artist_name = artistID.map(d => "C " + d);
-    //     var score_label = score.map(d=> "Score: " +d)
-    //     console.log(artist_name)
-        
-
-    //     var data = [{
-    //         x: artist_name,
-    //         y: score,
-    //         type: 'bar',
-    //         text: score_label
-    //     }]
-
-    //     var layout = {
-    //         title:'Top 10 Artists',
-    //         xaxis: { title: "Artist"},
-    //         yaxis: {title: "Rating"},
-    //         marker: {
-    //             color: 'lightblue'},
-    //         hovermode:'closest',
-    //         barmode: 'group'
-    //     };
-      
-    //     Plotly.newPlot('Artist_bar', data, layout);
-    // })    
- 
+function renderBarchart(selected) {
+    
+    // based on selected value change input for dynamic query
+    if (selected== 'Artist'){
+        input=0
+    }
+    else{
+        input=1
+    }
+    // console.log(input)
     var data = [], artist_info = {};
-    const query1 = '/api/v1.0/artistrating?limit=10'
+    const query1 = `/api/v1.0/artistrating?limit=10&is_band=${input}`
     d3.json(query1)
         .then(response1 => {
             data = response1;
@@ -60,10 +38,10 @@
             response2.forEach(artist => { artist_info[artist.id] = artist });
             // Draw chart with data. Use data['total_score'] for X-axis, data['artist_id'] for Y-axis, or the other way around
             // Add tooltips with artist_info, use artist_info[artist_id] to get the deatils
-            console.log(data['artist_id'].map(artist_id => artist_info[artist_id]))
+            // console.log(data['artist_id'].map(artist_id => artist_info[artist_id]))
 
             var data2 = [{
-                hovertemplate: `Artist: %{y} <br> Rating: %{x}`,
+                hovertemplate: `${selected}: %{y} <br>Rating: %{x}`,
                 x: data['total_score'].reverse(),
                 y: data['artist_id'].map(artist_id => artist_info[artist_id]['name']).reverse(),
                 type: 'bar',
@@ -81,3 +59,6 @@
           
             Plotly.newPlot('Artist_bar', data2, layout);
         });
+    }
+
+d3.select("#artistInput").on("change", handleInput);
